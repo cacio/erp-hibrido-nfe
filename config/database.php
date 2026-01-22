42446163d85696c8b224a42684a04a4ebb57d7ef
Cacio <caciorenato@gmail.com>
2026-01-22 18:07:47 -0300

    Ajuste no app version

--- config/database.php
+++ config/database.php
@@ -5,4 +5,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'erp_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('APP_VERSION', trim(shell_exec('git describe --tags')));
+define('APP_VERSION', 'v1.1.0');
